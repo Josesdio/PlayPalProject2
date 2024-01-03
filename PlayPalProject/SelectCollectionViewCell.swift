a8a34575c4002b9b8dc0327ecb36c79052b38f0e
josesdio <josesdio948@gmail.com>
2024-01-03 21:01:28 +0700

    finalPlayPal

---
+++ PlayPalProject/SelectCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SelectCollectionViewCell.swift
+//  PlayPalProject
+//
+//  Created by Josesdio on 11/12/23.
+//
+
+import UIKit
+
+class SelectCollectionViewCell: UICollectionViewCell {
+    
+}
